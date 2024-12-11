ae1b5d8d9b041a1bb0a2f0181827f04228c7372e
tcarvi-engenheiro-eduardo-leal <engenheiroeduardodossantosleal@gmail.com>
2024-12-10 22:28:11 -0300

    Update

--- website/docs/00-conceitos-gerais/00-conceitos.md
+++ website/docs/00-conceitos-gerais/00-conceitos.md
@@ -1,3 +1,32 @@
 # Conceitos
+
+## Tópicos de Estudo
 - Conjunto
-- Conjunto Numérico
+- Conjuntos Numéricos
+- Subconjuntos
+- Operações com conjuntos
+- Diagramas de Venn
+- Intervalos
+
+## Cardinalidade de Conjuntos
+- $ \text{card(A)} = o(A) = n(A)$
+	- sendo:
+		- $ A $ := um conjunto qualquer
+
+## Operações entre conjuntos
+- Symbools:
+![](img/set-logic-symbols.png)
+
+- Sendo:
+	- $ A = {1, 2, 3} $
+	- $ A = {2, 3, 4} $
+	- $ A = {3, 4, 5, 6} $
+
+### Union
+$ A $
+
+### Inersection
+$ $
+
+### Diference
+$ $
--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -1,6 +1,8 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -41,6 +43,8 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-teoria-dos-conjuntos/tree/main/',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         blog: {
           showReadingTime: true,
@@ -62,6 +66,16 @@ const config: Config = {
         },
       } satisfies Preset.Options,
     ],
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig: {
